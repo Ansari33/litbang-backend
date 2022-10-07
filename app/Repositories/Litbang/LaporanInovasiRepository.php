26b578e4459ddb58a490a33e47bd9ff2a83db2bc
Ansari33 <51727994+Ansari33@users.noreply.github.com>
2022-10-07 14:16:09 +0700

    Update LaporanInovasiRepository.php

--- app/Repositories/Litbang/LaporanInovasiRepository.php
+++ app/Repositories/Litbang/LaporanInovasiRepository.php
@@ -11,16 +11,42 @@ class LaporanInovasiRepository extends BaseRepository
     public function validate($request)
     {
         $validator = Validator::make($request->only(
-            'nomor',
-            'tanggal',
-            'nama',
-            'instansi'
+            'nama_skpd',
+            'nama_unit',
+            'nama_inovasi',
+            'nomor_sk_inovasi',
+            'tanggal_sk_inovasi',
+            'jumlah_pelaksana_inovasi',
+            'inisiator_daerah_inovasi',
+            'urusan_inovasi_daerah',
+            'waktu_uji_coba',
+            'waktu_diterapkan',
+            'rancang_bangun',
+            'tujuan',
+            'manfaat',
+            'hasil',
+            'jenis_inovasi',
+            'bentuk_inovasi',
+            'inovasi_tematik'
         ), [
-                'nomor' => 'required|unique:kelitbangan,nomor,NULL,id,deleted_at,NULL',
-
-                'tanggal' => 'required',
-                'instansi' => 'required',
-                'nama' => 'required'
+                'nomor' => 'required',
+                'nama_skpd' => 'required',
+                'nama_unit' => 'required',
+                'nama_inovasi' => 'required',
+                'nomor_sk_inovasi'  => 'required',
+                'tanggal_sk_inovasi'  => 'required',
+                'jumlah_pelaksana_inovasi'  => 'required',
+                'inisiator_daerah_inovasi'  => 'required',
+                'urusan_inovasi_daerah' => 'required',
+                'waktu_uji_coba' => 'required',
+                'waktu_diterapkan' => 'required',
+                'rancang_bangun' => 'required',
+                'tujuan' => 'required',
+                'manfaat' => 'required',
+                'hasil' => 'required',
+                'jenis_inovasi' => 'required',
+                'bentuk_inovasi' => 'required',
+                'inovasi_tematik' => 'required',
             ]
         );
         return $validator;
@@ -28,16 +54,42 @@ class LaporanInovasiRepository extends BaseRepository
     public function validateUpdate($request)
     {
         $validator = Validator::make($request->only(
-            'nomor',
-            'tanggal',
-            'instansi',
-            'nama'
+            'nama_skpd',
+            'nama_unit',
+            'nama_inovasi',
+            'nomor_sk_inovasi',
+            'tanggal_sk_inovasi',
+            'jumlah_pelaksana_inovasi',
+            'inisiator_daerah_inovasi',
+            'urusan_inovasi_daerah',
+            'waktu_uji_coba',
+            'waktu_diterapkan',
+            'rancang_bangun',
+            'tujuan',
+            'manfaat',
+            'hasil',
+            'jenis_inovasi',
+            'bentuk_inovasi',
+            'inovasi_tematik'
         ), [
                 'nomor' => 'required',
-                'nama' => 'required',
-                'tanggal' => 'required',
-                'instansi' => 'required',
-
+                'nama_skpd' => 'required',
+                'nama_unit' => 'required',
+                'nama_inovasi' => 'required',
+                'nomor_sk_inovasi'  => 'required',
+                'tanggal_sk_inovasi'  => 'required',
+                'jumlah_pelaksana_inovasi'  => 'required',
+                'inisiator_daerah_inovasi'  => 'required',
+                'urusan_inovasi_daerah' => 'required',
+                'waktu_uji_coba' => 'required',
+                'waktu_diterapkan' => 'required',
+                'rancang_bangun' => 'required',
+                'tujuan' => 'required',
+                'manfaat' => 'required',
+                'hasil' => 'required',
+                'jenis_inovasi' => 'required',
+                'bentuk_inovasi' => 'required',
+                'inovasi_tematik' => 'required',
             ]
         );
         return $validator;
